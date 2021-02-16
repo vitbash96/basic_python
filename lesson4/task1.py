e044fb94016666513a753f43223c6f345979ea1a
Vitalii <bashilov.vitas@icloud.com>
2021-02-16 15:04:17 +0200

    task1 solved

--- lesson4/task1.py
+++ lesson4/task1.py
@@ -1,1 +1,15 @@
-print('hello')
+import argparse
+
+parser = argparse.ArgumentParser(description="Pay calculation")
+parser.add_argument(
+    'hours', metavar='--hours', type=float, help='Number of hours worked'
+)
+parser.add_argument(
+    'wage', metavar='--wage', type=float, help='Hourly rate'
+)
+parser.add_argument(
+    '-bonus', metavar='--bonus', type=float, help='Bonus amount, if any', default=0
+)
+args = parser.parse_args()
+print(
+    f' The salary is {args.hours} hours * ${args.wage} per hour + ${args.bonus} bonus = {args.hours * args.wage + args.bonus}')
